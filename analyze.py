e47a13f4dc2a29ba6496f24a8179e57d267a6784
Scottie Enriquez <scott.c.enriquez@gmail.com>
2018-01-27 13:38:55 -0600

    Initial structure

---
+++ analyze.py
@@ -0,0 +1,3 @@
+import twitter
+
+twitterApi = twitter.Api(consumer_key=[consumer key], consumer_secret=[consumer secret], access_token_key=[access token], access_token_secret=[access token secret])
