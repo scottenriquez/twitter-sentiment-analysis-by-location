aba769733672f060f62cff2302d9dbef8cee83e0
Scottie Enriquez <scott.c.enriquez@gmail.com>
2018-01-27 16:56:45 -0600

    Working API call

--- analyze.py
+++ analyze.py
@@ -1,3 +1,11 @@
+import sys
 import twitter
 
-twitterApi = twitter.Api(consumer_key=[consumer key], consumer_secret=[consumer secret], access_token_key=[access token], access_token_secret=[access token secret])
+consumerKey = str(sys.argv[1])
+consumerSecretKey = str(sys.argv[2])
+accessKey = str(sys.argv[3])
+accessSecretKey = str(sys.argv[4])
+
+twitterApiClient = twitter.Api(consumer_key=consumerKey, consumer_secret=consumerSecretKey, access_token_key=accessKey, access_token_secret=accessSecretKey)
+results = twitterApiClient.GetSearch("q=twitter%20&result_type=recent&since=2018-01-21&count=100")
+print(results)
