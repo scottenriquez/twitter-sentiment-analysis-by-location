ac3031567e9a8fac9633bfeacf2837cb0fd45883
Scottie Enriquez <scott.c.enriquez@gmail.com>
2018-01-29 19:44:29 -0600

    Serializing tweets to output file; updating .gitignore for preferred output file name

--- analyze.py
+++ analyze.py
@@ -1,11 +1,30 @@
 import sys
 import twitter
+import jsonpickle
 
+#format command line input
 consumerKey = str(sys.argv[1])
 consumerSecretKey = str(sys.argv[2])
 accessKey = str(sys.argv[3])
 accessSecretKey = str(sys.argv[4])
+queryString = str(sys.argv[5])
+city = str(sys.argv[6])
+miles = str(sys.argv[7])
+outputFilePath = str(sys.argv[8])
 
+#execute Twitter search via RESTful API
 twitterApiClient = twitter.Api(consumer_key=consumerKey, consumer_secret=consumerSecretKey, access_token_key=accessKey, access_token_secret=accessSecretKey)
-results = twitterApiClient.GetSearch("q=twitter%20&result_type=recent&since=2018-01-21&count=100")
-print(results)
+#TODO: tinker with query format in advanced search URL
+formattedQuery = "q={0}".format(queryString, city, miles)
+tweets = twitterApiClient.GetSearch(formattedQuery)
+
+#write tweets to file
+jsonString = ""
+outputFile = open(outputFilePath, "w")
+for tweet in tweets:
+    jsonString += str(jsonpickle.encode(tweet))
+outputFile.write(jsonString)
+outputFile.close()
+
+#analyze tweets
+#TODO: execute command for b4msa
